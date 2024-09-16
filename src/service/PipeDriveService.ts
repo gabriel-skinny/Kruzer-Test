cf49a2c4126821a1d80e096e815d69854d51ceab
gabriel-skinny <gabrielcatoni12@gmail.com>
2024-09-16 19:56:25 -0300

    Finishgin PipeDriveService

--- src/service/PipeDriveService.ts
+++ src/service/PipeDriveService.ts
@@ -1,8 +1,12 @@
 import { IWebhookRepository } from "../database/repositories/webhookRepository";
 import { IWebhookDealUpdateData } from "../interfaces/webhookDealUpdate";
+import { IBillingService } from "./BlingService";
 
 export class PipeDriveService {
-  constructor(private webhookRepository: IWebhookRepository) {}
+  constructor(
+    private webhookRepository: IWebhookRepository,
+    private blingService: IBillingService
+  ) {}
 
   async handleUpdateDealEvent(data: IWebhookDealUpdateData) {
     await this.webhookRepository.save({
@@ -10,6 +14,12 @@ export class PipeDriveService {
       direction: "in",
       name: "PipeDrive",
       type: "updateDeal",
+    });
+
+    await this.blingService.insertProduct({
+      name: data.current.title,
+      quantity: data.current.products_count,
+      price: data.current.value,
     });
   }
 }
