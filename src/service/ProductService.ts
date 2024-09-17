876c2f0bb6c6cec86455c19170ef76090040cfc9
gabriel-skinny <gabrielcatoni12@gmail.com>
2024-09-17 17:51:25 -0300

    Changing service name

---
+++ src/service/ProductService.ts
@@ -0,0 +1,90 @@
+import { endOfDay, startOfDay } from "date-fns";
+import { IProductModel } from "../database/entities/product-entity";
+import { IProductAgregationRepository } from "../database/repositories/productAgregationRepository";
+import { IProductRepository } from "../database/repositories/productRepository";
+import { IInsertProductData } from "../integration/interface";
+import { IProductAgregationModel } from "../database/entities/product-agregation";
+
+export interface IInsertProductParams {
+  price: number;
+  name: string;
+  quantity: number;
+}
+
+interface IBlingHttp {
+  requestInsertProduct(data: IInsertProductData): Promise<{ id: string }>;
+}
+
+export class ProductService {
+  constructor(
+    private BlingHttp: IBlingHttp,
+    private productRepository: IProductRepository,
+    private productAgregationRepository: IProductAgregationRepository
+  ) {}
+
+  async insertProduct({ name, price, quantity }: IInsertProductParams) {
+    const { id } = await this.BlingHttp.requestInsertProduct({
+      nome: name,
+      preco: price,
+      formato: "S",
+      tipo: "P",
+    });
+
+    const productAgregation = await this.agregateProduct({ price, quantity });
+
+    await this.productRepository.save({
+      name,
+      price,
+      quantity,
+      externalId: id,
+      productAgregationId: productAgregation._id,
+    });
+  }
+
+  private async agregateProduct({
+    price,
+    quantity,
+  }: {
+    price: number;
+    quantity: number;
+  }) {
+    let productAgregation: IProductAgregationModel | null;
+
+    const starDateOfToday = startOfDay(Date.now());
+    const endDateOfToday = endOfDay(Date.now());
+    productAgregation =
+      await this.productAgregationRepository.findOnCreatedAtRange(
+        starDateOfToday,
+        endDateOfToday
+      );
+    if (productAgregation) {
+      const newSumValue = productAgregation.sumValue + price * quantity;
+
+      await this.productAgregationRepository.updateById({
+        id: productAgregation._id,
+        updateData: {
+          sumValue: newSumValue,
+          quantity: productAgregation.quantity + quantity,
+        },
+      });
+    } else {
+      const sumValue = price * quantity;
+      productAgregation = await this.productAgregationRepository.save({
+        quantity: quantity,
+        sumValue: sumValue,
+      });
+    }
+
+    return productAgregation;
+  }
+
+  async getProductAgregations(filter?: {
+    startDate: Date;
+    endDate: Date;
+  }): Promise<IProductAgregationModel[]> {
+    const products =
+      await this.productAgregationRepository.findManyOnCreatedAtRange(filter);
+
+    return products;
+  }
+}
