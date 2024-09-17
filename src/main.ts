582178c62254b7ff8d827d023091ad42c3d05016
gabriel-skinny <gabrielcatoni12@gmail.com>
2024-09-17 17:51:12 -0300

    Creating loader factory

---
+++ src/main.ts
@@ -0,0 +1,38 @@
+import { BlingController } from "./controllers/BlingController";
+import { PipeDriveController } from "./controllers/PipeDriveController";
+import { ProductController } from "./controllers/ProductController";
+import { ProductAgregationModel } from "./database/entities/product-agregation";
+import { ProductModel } from "./database/entities/product-entity";
+import { WebhookModel } from "./database/entities/webhook-entity";
+import { ProductAgregationRepository } from "./database/repositories/productAgregationRepository";
+import { ProductRepository } from "./database/repositories/productRepository";
+import { WebhookRepository } from "./database/repositories/webhookRepository";
+import { BlingIntegration } from "./integration/blingIntegration";
+import { PipeDriveService } from "./service/PipeDriveService";
+import { ProductService } from "./service/ProductService";
+
+export const makeControllersFactory = () => {
+  const blingIntegration = new BlingIntegration();
+
+  const productRepository = new ProductRepository(ProductModel);
+  const productAgregationRepository = new ProductAgregationRepository(
+    ProductAgregationModel
+  );
+  const productService = new ProductService(
+    blingIntegration,
+    productRepository,
+    productAgregationRepository
+  );
+
+  const webhookRepository = new WebhookRepository(WebhookModel);
+  const pipeDriveService = new PipeDriveService(
+    webhookRepository,
+    productService
+  );
+
+  const pipeDriveController = new PipeDriveController(pipeDriveService);
+  const blingController = new BlingController(blingIntegration);
+  const productController = new ProductController(productService);
+
+  return { pipeDriveController, blingController, productController };
+};
