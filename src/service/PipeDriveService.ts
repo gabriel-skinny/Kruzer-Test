ffa3d7b754f98d9aa00ea6d67f037b45672612b9
gabriel-skinny <gabrielcatoni12@gmail.com>
2024-09-18 17:38:21 -0300

    Removing retry method from pipeDriveService

--- src/service/PipeDriveService.ts
+++ src/service/PipeDriveService.ts
@@ -1,4 +1,8 @@
 import { IPipeDriveWebhookRepository } from "../database/repositories/pipeDriveWebhookRepository";
+import {
+  IGetProductsFromDealData,
+  IGetProductsFromDealResponse,
+} from "../integration/interface";
 import { IWebhookDealUpdateData } from "../interfaces/webhookDealUpdate";
 import { IInsertProductParams } from "./ProductService";
 
@@ -6,10 +10,15 @@ interface IProductService {
   insertProduct(data: IInsertProductParams): Promise<void>;
 }
 
+interface IPipeDriveIntegration {
+  getProductsFromDeal(dealId: string): Promise<IGetProductsFromDealData[]>;
+}
+
 export class PipeDriveService {
   constructor(
     private pipedriveWebhookRepository: IPipeDriveWebhookRepository,
-    private productService: IProductService
+    private productService: IProductService,
+    private pipeDriveIntegration: IPipeDriveIntegration
   ) {}
 
   async handleUpdateDealEvent({
@@ -22,62 +31,28 @@ export class PipeDriveService {
     if (await this.pipedriveWebhookRepository.existsByExternalId(meta.id))
       throw new Error("Duplicated webhook");
 
-    let errorCreatingProduct = false;
-    let errorCreatingProductMessage;
     if (data.status == "won" && previous.status == "open") {
       try {
-        const quantity = data.products_count || 1;
+        const productsOnDeal =
+          await this.pipeDriveIntegration.getProductsFromDeal(String(data.id));
 
-        await this.productService.insertProduct({
-          name: data.title,
-          quantity,
-          price: data.value,
-        });
+        for (const product of productsOnDeal) {
+          await this.productService.insertProduct({
+            id: String(product.id),
+            name: product.name,
+            quantity: product.quantity,
+            price: product.sum,
+          });
+        }
       } catch (error: any) {
-        errorCreatingProduct = true;
-        errorCreatingProductMessage = error.message;
-      }
-    }
-
-    await this.pipedriveWebhookRepository.save({
-      data,
-      type: "changeDeal",
-      errorOnProductCreation: errorCreatingProduct,
-      errorProductCreationMessage: errorCreatingProductMessage,
-      webhook_external_id: meta.id,
-    });
-  }
-
-  async retryProductCreation(): Promise<{
-    webhooksToRetry: number;
-    sucessCount: number;
-  }> {
-    const webhooksToRetry =
-      await this.pipedriveWebhookRepository.findManyWithErrorOnProductCreation();
-
-    let sucessCount = 0;
-    for (const webhook of webhooksToRetry) {
-      try {
-        await this.productService.insertProduct({
-          name: webhook.data.title,
-          quantity: webhook.data.products_count,
-          price: webhook.data.value,
-        });
-
-        await this.pipedriveWebhookRepository.updateById({
-          id: webhook._id,
-          updateData: { errorOnProductCreation: false },
-        });
-
-        sucessCount++;
-      } catch (error) {
         console.log(error);
       }
     }
 
-    return {
-      sucessCount,
-      webhooksToRetry: webhooksToRetry.length,
-    };
+    await this.pipedriveWebhookRepository.save({
+      dealData: data,
+      type: "changeDeal",
+      webhook_external_id: meta.id,
+    });
   }
 }
