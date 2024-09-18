d2b733c2a0ed0cc591f8de13cf7d004c04a9a5d6
gabriel-skinny <gabrielcatoni12@gmail.com>
2024-09-18 14:29:56 -0300

    Removing non used var

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,7 +1,5 @@
 import mongoose from "mongoose";
 
 export async function makeMongoConnection() {
-  const mongoConnection = await mongoose.connect(
-    process.env.MONGO_URL as string
-  );
+  await mongoose.connect(process.env.MONGO_URL as string);
 }
