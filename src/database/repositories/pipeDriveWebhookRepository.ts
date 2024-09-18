f35e9274f47f3d2a78cb5d75326d44fd3b7d81a7
gabriel-skinny <gabrielcatoni12@gmail.com>
2024-09-18 17:41:07 -0300

    Removing non used methods from pipedriveWebhookRepository

--- src/database/repositories/pipeDriveWebhookRepository.ts
+++ src/database/repositories/pipeDriveWebhookRepository.ts
@@ -6,14 +6,6 @@ export interface IPipeDriveWebhookRepository {
   existsByExternalId(
     externalId: string
   ): Promise<IPipeDriveWebhookModel | null>;
-  findManyWithErrorOnProductCreation(): Promise<IPipeDriveWebhookModel[]>;
-  updateById({
-    id,
-    updateData,
-  }: {
-    id: string;
-    updateData: Partial<IPipeDriveWebhookModel>;
-  }): Promise<void>;
 }
 
 export class WebhookRepository implements IPipeDriveWebhookRepository {
@@ -35,27 +27,5 @@ export class WebhookRepository implements IPipeDriveWebhookRepository {
     if (!webhook) return null;
 
     return webhook;
-  }
-
-  async findManyWithErrorOnProductCreation(): Promise<
-    IPipeDriveWebhookModel[]
-  > {
-    const webhooks = await this.pipeDriveWebhookModel.find({
-      errorOnProductCreation: true,
-    });
-
-    if (!webhooks) return [];
-
-    return webhooks;
-  }
-
-  async updateById({
-    id,
-    updateData,
-  }: {
-    id: string;
-    updateData: Partial<IPipeDriveWebhookModel>;
-  }): Promise<void> {
-    await this.pipeDriveWebhookModel.updateOne({ _id: id }, updateData);
   }
 }
