78a5fbbf9b7cf8eb2d4a0388377a75b43adebf7c
gabriel-skinny <gabrielcatoni12@gmail.com>
2024-09-18 17:40:30 -0300

    Adding new methods to product repository

--- src/database/repositories/productRepository.ts
+++ src/database/repositories/productRepository.ts
@@ -3,6 +3,15 @@ import { IProductModel } from "../entities/product-entity";
 
 export interface IProductRepository {
   save(data: Omit<IProductModel, "_id" | "createdAt">): Promise<void>;
+  findManyWithErrorOnCreation(): Promise<IProductModel[]>;
+  updateById({
+    id,
+    updateData,
+  }: {
+    id: string;
+    updateData: Partial<IProductModel>;
+  }): Promise<void>;
+  existsByPipeDriveExternalId(id: string): Promise<boolean>;
 }
 
 export class ProductRepository implements IProductRepository {
@@ -10,5 +19,31 @@ export class ProductRepository implements IProductRepository {
 
   async save(data: Omit<IProductModel, "_id" | "createdAt">): Promise<void> {
     await this.productModel.create(data);
+  }
+
+  async findManyWithErrorOnCreation(): Promise<IProductModel[]> {
+    const products = await this.productModel
+      .find({ errorOnCreation: true })
+      .lean();
+
+    if (!products) return [];
+
+    return products;
+  }
+
+  async existsByPipeDriveExternalId(id: string): Promise<boolean> {
+    const product = await this.productModel.exists({ pipeDriveExternalId: id });
+
+    return !!product;
+  }
+
+  async updateById({
+    id,
+    updateData,
+  }: {
+    id: string;
+    updateData: Partial<IProductModel>;
+  }): Promise<void> {
+    await this.productModel.updateOne({ _id: id }, updateData);
   }
 }
