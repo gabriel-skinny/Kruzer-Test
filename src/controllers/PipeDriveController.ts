f9690b2617a0549f22bc52e3b236bad76a27b0bc
gabriel-skinny <gabrielcatoni12@gmail.com>
2024-09-19 08:17:49 -0300

    Changing error status to PipeDrive webhook

--- src/controllers/PipeDriveController.ts
+++ src/controllers/PipeDriveController.ts
@@ -20,7 +20,7 @@ export class PipeDriveController {
     } catch (err) {
       console.log(err);
 
-      return res.status(500).json({ message: "Internal server error" });
+      return res.status(200).json({ message: "Internal server error" });
     }
   }
 }
